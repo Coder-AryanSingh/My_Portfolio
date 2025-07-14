c8a2a6232283587381eda34d5e9bd542a3386e54
Aryan Singh <asaryansingh09@gmail.com>
2025-07-14 23:46:50 +0530

    Add files via upload

---
+++ area_of_shapes.py
@@ -0,0 +1,22 @@
+r = float(input("Enter the radius of circle "))
+ac = 3.14*r*r    # area of circle
+print(ac)
+
+s = float(input("Enter the side of square "))
+sa = s*s         # area of square
+print(sa)
+
+l = float(input("Enter the length of rectangle "))
+b = float(input("Enter the breadth of rectangle "))
+al = l*b         # area of rectangle
+print(al)
+
+b = float(input("Enter the base of triangle "))
+h = float(input("Enter the height of triangle "))
+at = 1/2*b*h     # area of triangle
+print(at)
+
+b = float(input("Enter the base of parallelogram "))
+h = float(input("Enter the height of parallelogram "))
+ap = b*h         # area of parallelogram
+print(ap)
---
+++ list_practice.py
@@ -0,0 +1,11 @@
+
+# LISTS PRACTICE
+fruits = ["grapes","watermelon","mango",69,52.4,False]
+
+print(fruits[2])
+fruits[2] = "kiwi"
+                    
+print(fruits[2])
+# successfully done
+
+
